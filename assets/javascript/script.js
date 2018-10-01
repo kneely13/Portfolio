50db03b2965652162a0a1a11df9106ad1dc974cd
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-09-30 20:11:12 -0400

    added background some javascript , info changes, information itself stc.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,31 @@
-$(document).on(function(){
+$(document).ready(function() {
 
     $(".masthead").hide();
-    $("#timer").hide();
     $(".boxes").hide();
-
+    $("#footer").hide();
     
+    $("#startBtn").on("click", function() {
+        $(".intro").hide();
+        $(".masthead").hide();
+        $(".boxes").show();
+    })
+
+    $("#about").on("click", function() {
+        $(".intro").hide();
+        $(".masthead").show();
+        $(".boxes").hide();
+    })
+
+    $("#contact").on("click", function() {
+        $(".intro").hide();
+        $(".masthead").show();
+        $(".boxes").hide();
+    })
+
+    $("#projects").on("click", function() {
+        $(".intro").hide();
+        $(".masthead").show();
+        $(".boxes").hide();
+    })
+
 });
