a80e08af3485749e717e2ad52c3638e5bd582413
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-11-30 17:40:02 -0500

    image change

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+function touchCheck (){
 
     function hasTouch() {
         return 'ontouchstart' in document.documentElement
@@ -22,7 +22,8 @@ $(document).ready(function() {
             }
         } catch (ex) {}
     }
-    
+}
+
 myFunction= function() {
     var x = document.getElementById("navBar");
     if (x.className === "topnav") {
@@ -30,8 +31,13 @@ myFunction= function() {
     } else {
         x.className = "topnav";
     }
-    
 }
+
+
+$(document).ready(function() {
+    touchCheck();
+    myFunction(); 
+  
     $(".masthead").hide();
     $(".boxes").hide();
     $("#footer").hide();
@@ -71,3 +77,42 @@ myFunction= function() {
         
     })
 });
+
+
+
+
+
+function touchCheck (){
+
+    function hasTouch() {
+        return 'ontouchstart' in document.documentElement
+               || navigator.maxTouchPoints > 0
+               || navigator.msMaxTouchPoints > 0;
+    }
+    
+    if (hasTouch()) { // remove all :hover stylesheets
+        try { // prevent exception on browsers not supporting DOM styleSheets properly
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+    
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+    
+                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                        styleSheet.deleteRule(ri);
+                    }
+                }
+            }
+        } catch (ex) {}
+    }
+}
+
+myFunction= function() {
+    var x = document.getElementById("navBar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
